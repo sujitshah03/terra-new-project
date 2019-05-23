fff8e0639ddcf277e5e3afcd1756bbca6eab9761
Jay Shah <47807540+shahjayone123@users.noreply.github.com>
2019-05-23 11:49:52 -0400

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -6,3 +6,11 @@ resource "aws_subnet" "main" {
     Name = "my-subnet"
   }
 }
+resource "aws_subnet" "main2" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "my-subnet2"
+  }
+}
