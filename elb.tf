781a3187658863debe2cb1c39382eab6b2076400
Jay Shah <47807540+shahjayone123@users.noreply.github.com>
2019-05-23 00:04:22 -0400

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -2,6 +2,6 @@ resource "aws_lb" "myelb" {
   name               = "test-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.mysg}"]
+  security_groups    = ["${aws_security_group.mysg.name}"]
   subnets            = ["${aws_subnet.public.*.id}"]
 }
